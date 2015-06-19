340080a0a942644a986023a56b03ad4037693254
Florian Hirsch <fhi@adorsys.de>
2015-06-19 09:41:39 +0100

    added one link and hardcoded hode for running behind proxy

--- shop/lib/server.js
+++ shop/lib/server.js
@@ -5,11 +5,16 @@ var PORT = 8080;
 var PRERENDER_IP = process.env.PRERENDERSHOP_PRERENDER_1_PORT_3000_TCP_ADDR;
 var PRERENDER_PORT = process.env.PRERENDER_1_PORT_3000_TCP_PORT;
 
+var HOST = process.env.PUBLIC_HOST_ADDRESS;
+
 var prerenderServiceUrl = 'http://' + PRERENDER_IP + ':' + PRERENDER_PORT;
 
 var app = express();
 
-app.use(require('prerender-node').set('prerenderServiceUrl', prerenderServiceUrl));
+app.use(require('prerender-node')
+    .set('prerenderServiceUrl', prerenderServiceUrl)
+    .set('host', HOST)
+    );
 app.use('/bower_components', express.static('/usr/src/app/bower_components'));
 app.use('/app', express.static('/usr/src/app/lib/app'));
 
@@ -19,4 +24,4 @@ app.get('/', function(req, res){
 
 app.listen(PORT);
 
-console.log('angular app running on http://YOUR_DOCKER_IP:' + PORT + ' using prerender service on ' + prerenderServiceUrl);
+console.log('angular app running on http://' + HOST + ' using prerender service on ' + prerenderServiceUrl);
